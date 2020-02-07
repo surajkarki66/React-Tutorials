4e4fff4ba7c4d6f4f0f7be84fe96829c166fe56d
surajkarki66 <suraj.karki500@gmail.com>
2020-02-07 17:06:19 +0545

    Tracked

--- react-complete-tutorial/src/App.js
+++ react-complete-tutorial/src/App.js
@@ -1,22 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person.js';
-import styled from 'styled-components';
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-    color: black;
-  }
-
-`;
 
 class App extends Component {
   state = {
