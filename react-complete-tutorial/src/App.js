247cb1fd50afc2f96a38b94299ea941ce3efd531
surajkarki66 <suraj.karki500@gmail.com>
2020-02-07 16:13:15 +0545

    Finished Radium

--- react-complete-tutorial/src/App.js
+++ react-complete-tutorial/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person.js';
 
 class App extends Component {
   state = {
     persons: [
       {id: 'akljjf', name: 'Michael Karki', age: 20},
-      {id: 'allja', name: 'Suraj Karki', age: 20}
+      {id: 'allja', name: 'Saman Shrestha', age: 20},
+      {id: 'ksfj', name: 'Binish Joshi', age: 20}
     ],
     showPersons: false
   }
@@ -41,11 +43,16 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white', 
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
     let persons = null;
     if (this.state.showPersons) {
@@ -61,20 +68,35 @@ class App extends Component {
              />
           })}
         </div>
-      )
+      );
+       style.backgroundColor = 'red';
+       style[':hover'] = {
+         backgroundColor: 'salmon',
+         color: 'black'
+       }
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');  // classes = ['red']
+    }
+    if (this.state.persons.length <=1) {
+      classes.push('bold'); // classes = ['red', 'bold']
     }
     return (
+      <StyleRoot>
       <div className="App">
         <h1>This is my journey to React</h1>
-        <h2>This is Working</h2>
+        <h2 className={classes.join(' ')}>This is Working</h2>
         <button
          style={style}
          onClick={this.togglePersonHandler}>Toggle Name</button>  
          {persons}      
       </div>
+      </StyleRoot>
       
     );
   }
 }
 
-export default App;
+export default Radium(App);
--- react-complete-tutorial/src/Person/Person.js
+++ react-complete-tutorial/src/Person/Person.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
 import './Person.css';
-
+import Radium from 'radium';
 
 const person = (props) => {
+    const style = {
+        '@media (min-width: 500px)' : {
+            width: '450px'
+        }
+    }
     return (
-        <div className="Person">
+        <div className="Person" style={style}>
              <p onClick={props.click}>I am {props.name} and i am {props.age} years old.</p>
              <p>{props.children}</p>
              <input type="text" onChange={props.changed} value={props.name}/>
@@ -14,4 +18,4 @@ const person = (props) => {
     )
 }
 
-export default person;
+export default Radium(person);
