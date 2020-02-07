8d12e119895001eccc40591492437b12e2226290
surajkarki66 <suraj.karki500@gmail.com>
2020-02-07 21:50:56 +0545

    Finished error

--- react-complete-tutorial/src/App.js
+++ react-complete-tutorial/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person.js';
+import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -47,13 +48,11 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
+            return<ErrorBoundary key = {person.id}><Person
              click = {this.deletePersonHandler.bind(this, index)} 
              name={person.name}
              age={person.age}
-             key = {person.id}
-             changed = {(event) => this.nameChangedHandler(event, person.id)}
-             />
+             changed = {(event) => this.nameChangedHandler(event, person.id)}/></ErrorBoundary>
           })}
         </div>
       );
