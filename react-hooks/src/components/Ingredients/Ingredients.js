f71122c188bec82b5c0d457002a3433f14024058
surajkarki66 <suraj.karki500@gmail.com>
2020-05-19 22:47:21 +0545

    Started custom hooks

--- react-hooks/src/components/Ingredients/Ingredients.js
+++ react-hooks/src/components/Ingredients/Ingredients.js
@@ -21,30 +21,10 @@ const ingredientReducer = (currentIngredient, action) => {
   }
 };
 
-// Another reducer
-const httpReducer = (curHttpState, action) => {
-  switch (action.type) {
-    case 'SEND':
-      return { loading: true, error: null};
-    case 'RESPONSE':
-      return {...curHttpState,  loading: false};
-    case 'ERROR':
-      return { loading: false, error: action.errorMessage};
-    case 'CLEAR':
-      return {...curHttpState, error: null};
-    default:
-      throw new Error('Should not be reached!');
-  }
-}
-
-
-
 const Ingredients = () =>  {
   // Initializing useReducer
   const [userIngredients, dispatch] = useReducer(ingredientReducer, []); // for first time
-  const [httpState, dispatchHttp] = useReducer(httpReducer,  {loading: false, error: null});
-
-
+  
   //const [userIngredients, setUserIngredients ] = useState([]);
   //const [isLoading, setIsLoading] = useState(false);
   //const [error, setError] = useState();
