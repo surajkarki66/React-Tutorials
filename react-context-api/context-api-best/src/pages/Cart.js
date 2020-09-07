ce25a0ea81dbb63bc08ce03066eef61ad7794900
Suraj Karki <suraj.karki500@gmail.com>
2020-09-07 22:05:36 +0545

    Finished cart component

---
+++ react-context-api/context-api-best/src/pages/Cart.js
@@ -0,0 +1,48 @@
+import React, { useContext, useEffect } from "react";
+
+import ShopContext from "../context/shop-context";
+import MainNavigation from "../components/MainNavigation";
+import "./Cart.css";
+
+const CartPage = () => {
+  const context = useContext(ShopContext);
+
+  useEffect(() => {
+    console.log(context);
+  }, []);
+
+  return (
+    <React.Fragment>
+      <MainNavigation
+        cartItemNumber={context.cart.reduce((count, curItem) => {
+          return count + curItem.quantity;
+        }, 0)}
+      />
+      <main className="cart">
+        {context.cart.length <= 0 && <p>No Item in the Cart!</p>}
+        <ul>
+          {context.cart.map((cartItem) => (
+            <li key={cartItem.id}>
+              <div>
+                <strong>{cartItem.title}</strong> - ${cartItem.price} (
+                {cartItem.quantity})
+              </div>
+              <div>
+                <button
+                  onClick={context.removeProductFromCart.bind(
+                    this,
+                    cartItem.id
+                  )}
+                >
+                  Remove from Cart
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </React.Fragment>
+  );
+};
+
+export default CartPage;
