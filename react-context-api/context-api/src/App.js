1fe3dd4dacfb1154d43d622899a7bd20c422bdb0
Suraj Karki <suraj.karki500@gmail.com>
2020-09-07 12:53:25 +0545

    Created empty context object

--- react-context-api/context-api/src/App.js
+++ react-context-api/context-api/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Download Image</h1>
+    </div>
+  );
 }
 
 export default App;
