8117773238a56cc92818e9f4fd90eb5312b49c89
Suraj Karki <suraj.karki500@gmail.com>
2020-09-07 21:26:18 +0545

    Created GlobalState

---
+++ react-context-api/context-api-best/src/context/GlobalState.js
@@ -0,0 +1,42 @@
+import React, { useReducer } from "react";
+
+import ShopContext from "./shop-context";
+import { shopReducer, ADD_PRODUCT, REMOVE_PRODUCT } from "./reducers";
+
+const GlobalState = (props) => {
+  const products = [
+    { id: "p1", title: "Gaming Mouse", price: 29.99 },
+    { id: "p2", title: "Harry Potter 3", price: 9.99 },
+    { id: "p3", title: "Used plastic bottle", price: 0.99 },
+    { id: "p4", title: "Half-dried plant", price: 2.99 },
+  ];
+
+  const [cartState, dispatch] = useReducer(shopReducer, { cart: [] });
+
+  const addProductToCart = (product) => {
+    setTimeout(() => {
+      dispatch({ type: ADD_PRODUCT, product: product });
+    }, 700);
+  };
+
+  const removeProductFromCart = (productId) => {
+    setTimeout(() => {
+      dispatch({ type: REMOVE_PRODUCT, productId: productId });
+    }, 700);
+  };
+
+  return (
+    <ShopContext.Provider
+      value={{
+        products: products,
+        cart: cartState.cart,
+        addProductToCart: addProductToCart,
+        removeProductFromCart: removeProductFromCart,
+      }}
+    >
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
+
+export default GlobalState;
