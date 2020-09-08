1505b9842a2fea029024391b2c5d5b7354f77296
Suraj Karki <suraj.karki500@gmail.com>
2020-09-08 17:09:24 +0545

    Added removeEmployee in reducer

--- react-context-api/context-api-practice/src/context/employeeReducer.js
+++ react-context-api/context-api-practice/src/context/employeeReducer.js
@@ -6,6 +6,14 @@ export default (state, action) => {
         employees: [...state.employees, action.payload],
       };
 
+    case "REMOVE_EMPLOYEES":
+      return {
+        ...state,
+        employees: state.employees.filter(
+          (employee) => employee.id !== action.payload
+        ),
+      };
+
     default:
       return state;
   }
