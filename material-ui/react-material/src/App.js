26c934bed91e634bacb252fad312a141faa26136
Suraj Karki <suraj.karki500@gmail.com>
2020-09-09 19:10:32 +0545

    Added mu AppBar component

--- material-ui/react-material/src/App.js
+++ material-ui/react-material/src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar color="primary" position="static">
+        <h1>My Header</h1>
+      </AppBar>
     </div>
   );
 }
