fc20fe15c8ff6dd6339baca9752eccbaff7e8114
Suraj Karki <suraj.karki500@gmail.com>
2020-09-10 21:57:46 +0545

    Added makeStyles hook

--- material-ui/react-material-practice/src/App.js
+++ material-ui/react-material-practice/src/App.js
@@ -1,13 +1,31 @@
 import React from "react";
-import { Button } from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 import "./App.css";
 
+const useStyles = makeStyles({
+  helloStyles: {
+    fontStyle: "oblique",
+    color: "orange",
+  },
+  buttonStyles: {
+    color: "black",
+  },
+});
+
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <h1>This is my Button component</h1>
-      <Button color="secondary" fullWidth="true" variant="outlined">
+      <Typography className={classes.helloStyles} variant="h1" color="primary">
+        This is my Button component
+      </Typography>
+      <Button
+        className={classes.buttonStyles}
+        color="secondary"
+        variant="outlined"
+      >
         Button
       </Button>
     </div>
