5a027772c2078227254ae5b4999311172782c5f8
Suraj Karki <suraj.karki500@gmail.com>
2020-09-16 13:04:47 +0545

    Finished Grid and Grid layout

---
+++ material-ui/react-material-practice-2/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+import FullWidthGrid from "./Practice/Grid";
+function App() {
+  return (
+    <div className="App">
+      <FullWidthGrid />
+    </div>
+  );
+}
+
+export default App;
