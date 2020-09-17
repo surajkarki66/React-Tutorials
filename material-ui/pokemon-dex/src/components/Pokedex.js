7afe988449d219667e848cc4bcd80fa8abb38c92
Suraj Karki <suraj.karki500@gmail.com>
2020-09-17 17:38:42 +0545

    Finished Grid and Grid layout

--- material-ui/pokemon-dex/src/components/Pokedex.js
+++ material-ui/pokemon-dex/src/components/Pokedex.js
@@ -1,5 +1,34 @@
 import React from "react";
+import { AppBar, Toolbar, Grid, Card, CardContent } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+  pokedexContainer: {
+    paddingTop: "20px",
+    paddingLeft: "50px",
+    paddingRight: "50px",
+  },
+});
+
+const getPokemonCard = () => {
+  return (
+    <Grid item xs={12} sm={4}>
+      <Card>
+        <CardContent>Hi</CardContent>
+      </Card>
+    </Grid>
+  );
+};
 export default function PokeDex() {
-  return <div>This is the PokeDex</div>;
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <AppBar position="static">
+        <Toolbar />
+      </AppBar>
+      <Grid className={classes.pokedexContainer} container spacing={2}>
+        {getPokemonCard()}
+      </Grid>
+    </React.Fragment>
+  );
 }
